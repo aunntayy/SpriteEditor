290857c4a63028af087628228d55f446bbc15e2b
aunntayy <chanhkone@gmail.com>
2024-11-06 19:03:34 -0700

    Have the color tab UI running. Able to changed color base on slider or hex value. Able to custom color.

--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,10 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QLineEdit>
 #include <QMainWindow>
+#include <QPushButton>
+#include <QSlider>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -9,15 +12,32 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    void colorUI(QWidget *colorsTab);
+
 private:
     Ui::MainWindow *ui;
+
+    // Color Manager
+    QVector<QVector<QPushButton *>> colorButtons;
+    QSlider *redSlider;
+    QSlider *blueSlider;
+    QSlider *greenSlider;
+    QLineEdit *hexLineEdit;
+    QPushButton *brushColor;
+    QMap<QPushButton*, QColor> customColors;
+
+
+private slots:
+    void updateColorOnSlider();
+    void updateColorOnHexCode();
+    void updateColorCustomPalette();
+
 };
 #endif // MAINWINDOW_H
